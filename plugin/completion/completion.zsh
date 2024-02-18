c925b8ca8a58bd42268b5166728215db86be1854
Goldeneye128 <christian.devik@icloud.com>
2024-02-18 18:07:24 +0100

    added color options for completions

--- plugin/completion/completion.zsh
+++ plugin/completion/completion.zsh
@@ -11,5 +11,22 @@ bindkey -M menuselect '^[[Z' reverse-menu-complete
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
+# Detect the operating system
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS specific commands
+  # Setup LS_COLORS equivalent for macOS
+  export CLICOLOR=1
+  export LSCOLORS=ExFxCxDxBxegedabagaced
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Linux specific commands
+  # Use dircolors if available to setup LS_COLORS
+  if [[ -f ~/.dircolors ]]; then
+    eval "$(dircolors -b ~/.dircolors)"
+  else
+    dircolors -p > "$HOME"/.dircolors
+    eval "$(dircolors -b)"
+  fi
+fi
+
 # color setup
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
